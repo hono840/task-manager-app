77ad8713d13f6981113a7d1a80243cdd8966b246
hiro <>
2025-02-02 11:03:30 +0900

    サインアップ機能作成

--- src/app/components/molecules/Form.tsx
+++ src/app/components/molecules/Form.tsx
@@ -1,8 +1,14 @@
 import React, { FC, PropsWithChildren } from 'react'
 
-const Form = ({ children }: PropsWithChildren) => {
+type Props = {
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void
+}
+
+const Form = ({ children, onSubmit }: PropsWithChildren<Props>) => {
   return (
-    <form className="bg-gray-800 rounded-lg p-4 space-y-4 w-full">
+    <form
+      className="bg-gray-800 rounded-lg p-4 space-y-4 w-full"
+      onSubmit={onSubmit}>
       {children}
     </form>
   )
--- src/app/components/templates/SignUpTemplate.tsx
+++ src/app/components/templates/SignUpTemplate.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import PageWrapper from '../molecules/PageWrapper'
 import ContentsWrapper from '../molecules/ContentsWrapper'
 import SectionTitle from '../atoms/SectionTitle'
@@ -7,22 +9,54 @@ import Form from '../molecules/Form'
 import InputArea from '../molecules/InputArea'
 import PrimaryButton from '../atoms/Buttons/PrimaryButton'
 import AuthSwitchLink from '../organisms/AuthSwitchLink'
+import { supabase } from '@/app/utils/supabaseClient';
 
 const SignUpTemplate = () => {
+  const [userName, setUserName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onChangeUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value)
+  }
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+  }
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+  // サインアップ処理
+  const signUp = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const { error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        options: {
+          data: { userName }
+        }
+      })
+      if (error) {
+        alert('サインアップに失敗しました')
+        return;
+      }
+      alert('サインアップに成功しました、認証メールを送ったのでご確認ください')
+    } catch (err) {
+      console.error(err)
+    }
+  }
   return (
     <PageWrapper>
       <ContentsWrapper className='max-w-md'>
         <SectionTitle>Sign Up</SectionTitle>
         <Section>
-          {/* ページ見出し */}
-          {/* ユーザー登録フォーム */}
-          <Form>
+          <Form onSubmit={signUp}>
             <InputArea
               htmlFor="name"
               id="name"
               labelName="ユーザーネーム"
-              // onChange={onChange}
-              // value={value}
+              onChange={onChangeUserName}
+              value={userName}
               type="text"
               placeholder="Your Name"
             />
@@ -30,8 +64,8 @@ const SignUpTemplate = () => {
               htmlFor="email"
               id="email"
               labelName="メールアドレス"
-              // onChange={onChange}
-              // value={value}
+              onChange={onChangeEmail}
+              value={email}
               type="text"
               placeholder="your-email@example.com"
             />
@@ -39,8 +73,8 @@ const SignUpTemplate = () => {
               htmlFor="password"
               id="password"
               labelName="パスワード"
-              // onChange={onChange}
-              // value={value}
+              onChange={onChangePassword}
+              value={password}
               type="password"
               placeholder="••••••••"
             />
